ffb49bd4b7f5b896e94edf2164e0b57784c1043e
mixalis340 <mioannoug@csd.auth.gr>
2022-08-28 13:27:38 +0300

    -

--- src/main/kotlin/com/example/util/Constants.kt
+++ src/main/kotlin/com/example/util/Constants.kt
@@ -5,7 +5,7 @@ object Constants {
 
    const val DATABASE_NAME = "pose-detection"
 
-    const val BASE_URL = "http://10.0.2.2:8080/"
+    const val BASE_URL = "http://192.168.1.6:8080/"
     const val PROFILE_PICTURE_PATH = "build/resources/main/static/profile_pictures/"
     const val DEFAULT_PROFILE_PICTURE_PATH = "${BASE_URL}profile_pictures/avatar.svg"
 }
